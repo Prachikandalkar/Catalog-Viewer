c036bb00ad553d199c738fbfc9f6f29e0f670c32
prachi <prachikandalkar.hdfcergo@gmail.com>
2023-05-20 13:02:22 +0530

    catalogviewer

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const people = [
+    {
+        id: 1,
+        image: "/images/jennie.jpg",
+        name: "Jennie",
+        title: "Rapper",
+        quote: "Born and raised in South Korea, Jennie studied in New Zealand for five years before returning to South Korea in 2010. She debuted as a member of the girl group Blackpink, formed by YG Entertainment, in August 2016."
+    },
+    {
+        id: 2,
+        image: "/images/jisso.jpg",
+        name: "Jisso",
+        title: "Visuals",
+        quote: "Jisoo was born on January 3, 1995 in Gunpo. She's a member of Blackpink. She became a YG Entertainment trainee in August 2011 & trained for 5 years. In August 2016, she became a member of Blackpink, as a lead vocalist & visualist in the group."
+    },
+    {
+        id: 3,
+        image: "/images/lisa.jpg",
+        name: "lisa",
+        title: "Dancer",
+        quote: "She is based in South Korea and goes by the stage name Lisa. She is known as a member of Blackpink, the highest-charting female k-pop group. In 2021, she released her first solo single album named LALISA, which was a huge success. Lisa was born in Thailand on 27 March 1997."
+    },
+    {
+        id: 4,
+        image: "/images/rose.jpg",
+        name: "Rose",
+        title: "Singer",
+        quote: "Park Chaeyoung, better known by her English name Roseanne Park (born February 11, 1997 in Auckland, New Zealand) or Rosé, is the main vocalist and lead dancer of South Korean girl group BLACKPINK."
+    },
+];
+
+export default people;
